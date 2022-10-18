36134b6e5d7953a425004cc847355ef03bba223b
GrossoLuis <luisenriquegrosso@gmail.com>
2022-10-18 14:16:43 -0300

    modal background y navegacion arreglados

--- my-agencia-pouer/src/components/planes/planes.jsx
+++ my-agencia-pouer/src/components/planes/planes.jsx
@@ -73,7 +73,9 @@ export default function Planes() {
             <section className={s.sectionAsesorarme}>
                 <h1 className={s.titleAsesorarme}>Si no encuentras lo pagina que se adapte a tus <br /> necesidades no dudes en consultarnos</h1>
                 <hr className={s.hrAsesorarme} />
+                <Link to={'/contacto'}>
                 <button className={s.btnAsesorarme}>Asesorarme</button>
+                </Link>
             </section>
         </>
     )
